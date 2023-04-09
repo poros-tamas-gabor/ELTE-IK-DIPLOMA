f46b2e3ea732cdd098729e5a139c17999c72dbd1
poros-tamas-gabor <57863207+poros-tamas-gabor@users.noreply.github.com>
2023-04-09 15:00:57 +0200

    Update COMException.h

--- TerrainRender/TerrainRender/TerrainRender/COMException.h
+++ TerrainRender/TerrainRender/TerrainRender/COMException.h
@@ -5,21 +5,12 @@
 #include "StringConverter.h"
 #include <string>
 
-#define THROW_COM_EXCEPTION_IF_FAILED(hr, msg) if (FAILED (hr)) throw COMException( hr, msg, __FILE__, __FUNCTION__, __LINE__ )
+#define THROW_COM_EXCEPTION_IF_FAILED(hr, msg) if (FAILED (hr)) throw COMException( hr, msg, __FILEW__, __FUNCTIONW__, __LINE__ )
 class COMException
 {
 private:
 	std::wstring m_whatmsg;
 public:
-	COMException(HRESULT hr, const std::wstring& msg, const std::string& file, const std::string& function, int line)
-	{
-		_com_error error(hr);
-		m_whatmsg = L"Msg: " + msg + L"\n";
-		m_whatmsg += error.ErrorMessage();
-		m_whatmsg += L"\nFile: " + StringConverter::StringToWide(file);
-		m_whatmsg += L"\nFunction: " + StringConverter::StringToWide(function);
-		m_whatmsg += L"\nLine: " + StringConverter::StringToWide(std::to_string(line));
-	}
 
 	COMException(HRESULT hr, const std::wstring& msg, const std::wstring& file, const std::wstring& function, int line)
 	{
@@ -37,4 +28,3 @@ public:
 	}
 };
 #endif // !COM_EXCEPTION_H
-
